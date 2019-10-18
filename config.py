eba4600698bfadb28b46fb490037f23cd5f43524
pengbiaobiao <pengbiaobiao@yuehuikang.com>
2019-10-18 15:16:23 +0800

    调整邮件功能

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ CSRF_ENABLED = True
 SECRET_KEY = 'go-fuck-yourself'
 
 # Flask-SQLAlchemy 配置
-SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@192.168.119.132:3306/SablierBlog'
+# SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@192.168.119.132:3306/SablierBlog'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/SablierBlog'
 # SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_ECHO = True
@@ -19,7 +20,7 @@ MAIL_SERVER = 'smtp.163.com'  # 发邮件主机
 MAIL_PORT = 465  # 发邮件端口
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
-MAIL_USERNAME = 'hmmeiduo@163.com'  # 授权的邮箱
-MAIL_PASSWORD = 'hmmeiduo123'  # 邮箱授权时获得的密码，非注册登录密码
+MAIL_USERNAME = 'Mrsablier@163.com'  # 授权的邮箱
+MAIL_PASSWORD = 'Sablieradmin183'  # 邮箱授权时获得的密码，非注册登录密码
 MAIL_SUBJECT_PREFIX = 'SablierBlog'
-MAIL_DEFAULT_SENDER = 'SablierBlog<hmmeiduo@163.com>'
+MAIL_DEFAULT_SENDER = 'SablierBlog<MrSablier@163.com>'
